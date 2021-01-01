5e8a71378008247999eaa4670dbce9651c331603
Daniel-Alencar <danielalencar746@gmail.com>
2021-01-01 11:04:01 -0300

    Começo de construção da tela inicial

--- App.tsx
+++ App.tsx
@@ -1,11 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ScrollView style={styles.contentContainer}>
+
+        <View style={styles.playlistsContainer}>
+          <Text style={styles.titles}>Mais ouvidas</Text>
+          <View style={styles.playlistsView}>
+            <ScrollView horizontal style={styles.playlists}>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+
+        <View style={styles.playlistsContainer}>
+          <Text style={styles.titles}>Mais ouvidas</Text>
+          <View style={styles.playlistsView}>
+            <ScrollView horizontal style={styles.playlists}>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+
+        <View style={styles.playlistsContainer}>
+          <Text style={styles.titles}>Mais ouvidas</Text>
+          <View style={styles.playlistsView}>
+            <ScrollView horizontal style={styles.playlists}>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+
+        <View style={styles.playlistsContainer}>
+          <Text style={styles.titles}>Mais ouvidas</Text>
+          <View style={styles.playlistsView}>
+            <ScrollView horizontal style={styles.playlists}>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+              <View style={styles.playlist}>
+
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </ScrollView>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +96,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  contentContainer: {
+    borderColor: '#fff',
+    borderWidth: 0,
+    marginTop: 0,
+    height: '100%',
+    width: '100%',
+    padding: '6%'
+  },
+  playlistsContainer: {
+    borderColor: '#fff',
+    borderWidth: 0,
+    height: '33%'
+  },
+  titles: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  playlistsView: {
+    height: 150,
+  },
+  playlists: {
+    borderColor: '#555',
+    borderWidth: 0,
+    width: '100%',
+  },
+  playlist: {
+    borderColor: '#555',
+    borderWidth: 1,
+    width: 150,
+    height: 150,
+    marginRight: 10,
   },
 });
