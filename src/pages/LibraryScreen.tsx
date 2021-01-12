3f1f8efccae93fdbf317440b36b908d5f86966da
Daniel-Alencar <danielalencar746@gmail.com>
2021-01-12 11:22:49 -0300

    FAzendo componente SearchBar

--- src/pages/LibraryScreen.tsx
+++ src/pages/LibraryScreen.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { ScrollView, View, Dimensions, StyleSheet } from 'react-native';
+import { ScrollView, View, Dimensions, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
+
 import NavigationBar from '../components/NavigationBar';
 
 export default function LibraryScreen() {
@@ -8,6 +11,18 @@ export default function LibraryScreen() {
 
       <View style={styles.contentContainerView}>
         <ScrollView style={styles.contentContainer}>
+          
+          <View style={styles.searchBarContainer} >
+             <TextInput
+              returnKeyType="search"
+              style={styles.searchBar}
+              placeholder={"Que música você quer ouvir?"}
+            />
+            
+            <TouchableOpacity style={styles.xButton}>
+              <Feather name="x" size={30} color="#f00"/>
+            </TouchableOpacity>
+          </View>
 
         </ScrollView>
       </View>
@@ -41,5 +56,39 @@ const styles = StyleSheet.create({
     padding: '0%',
     paddingTop: '1%',
     paddingBottom: '1%',
+
+  },
+
+  searchBarContainer: {
+    borderWidth: 1,
+    borderColor: '#fff',
+
+    backgroundColor: '#000000aa',
+    width: '100%',
+    height: 90,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchBar: {
+    backgroundColor: '#B7A9A7',
+    width: '70%',
+    height: 50,
+
+    borderBottomLeftRadius: 10,
+    borderTopLeftRadius: 10,
+    padding: 10,
+  },
+  xButton: {
+    backgroundColor: '#B7A9A7',
+    width: 40,
+    height: 50,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderBottomEndRadius: 10,
+    borderTopEndRadius: 10,
   },
 });
