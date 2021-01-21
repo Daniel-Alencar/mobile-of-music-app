fb2a84e08e60b21fe519e7effec45bb467d2ed5a
Daniel-Alencar <danielalencar746@gmail.com>
2021-01-21 14:03:35 -0300

    Simplificação das Telas da libraryScreen

--- src/pages/ArtistsScreen.tsx
+++ src/pages/ArtistsScreen.tsx
@@ -1,48 +1,27 @@
 import React from 'react';
-import { ScrollView, View, Dimensions, StyleSheet } from 'react-native';
 
+import DefaultScreen from '../components/DefaultScreen';
 import PlusButton from '../components/PlusButton';
 import ArtistContainer from '../components/Salvos/ArtistContainer';
 
 export default function ArtistsScreen() {
   return (
-    <View style={styles.container}>
-
-      <View style={styles.contentContainerView}>
-        <ScrollView style={styles.contentContainer}>
-
-          <ArtistContainer 
-            artistName="John Mayer" 
-            imageSource={require('../images/Artists/JohnMayer.png')}
-          />
-          <ArtistContainer 
-            artistName="Daniel Alencar" 
-          />
-
-        </ScrollView>
-      </View>
+    <>
+      <DefaultScreen 
+        content={
+          <>
+            <ArtistContainer 
+              artistName="John Mayer" 
+              imageSource={require('../images/Artists/JohnMayer.png')}
+            />
+            <ArtistContainer 
+              artistName="Daniel Alencar" 
+            />
+          </>
+        }
+      />
 
       <PlusButton />
-
-    </View>
+    </>
   );
 }
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#000',
-    height: '100%',
-    width: windowWidth,
-  },
-  contentContainerView: {
-    borderColor: '#00f',
-    borderWidth: 0,
-  },
-  contentContainer: {
-    borderColor: '#0f0',
-    borderWidth: 0,
-  },
-});
--- src/pages/DownloadsScreen.tsx
+++ src/pages/DownloadsScreen.tsx
@@ -1,55 +1,34 @@
 import React from 'react';
-import {View, Dimensions, StyleSheet, ScrollView } from 'react-native';
 
 import MusicContainer from '../components/Salvos/MusicContainer';
 import PlusButton from '../components/PlusButton';
+import DefaultScreen from '../components/DefaultScreen';
 
 export default function DownloadsScreen() {
   return (
-    <View style={styles.container}>
-
-      <View style={styles.contentContainerView}>
-        <ScrollView style={styles.contentContainer}>
-
-          <MusicContainer 
+    <>
+      <DefaultScreen 
+        content={
+          <>
+            <MusicContainer 
             musicName="Gravity" 
             imageSource={require('../images/Playlists/Continuum.jpg')}
             centerTextMusic={true} 
-          />
-          <MusicContainer 
-            musicName="In Repair" 
-            imageSource={require('../images/Playlists/Continuum.jpg')}
-            centerTextMusic={true}  
-          />
-          <MusicContainer 
-            musicName="Ao som do mar"
-            centerTextMusic={true}  
-          />
+            />
+            <MusicContainer 
+              musicName="In Repair" 
+              imageSource={require('../images/Playlists/Continuum.jpg')}
+              centerTextMusic={true}  
+            />
+            <MusicContainer 
+              musicName="Ao som do mar"
+              centerTextMusic={true}  
+            />
+          </>
+        }
+      />
 
-        </ScrollView>
-      </View>
-
-      <PlusButton onPress={() => {}} />
-
-    </View>
+      <PlusButton />
+    </>
   );
 }
-
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#000',
-    height: '100%',
-    width: windowWidth,
-  },
-  contentContainerView: {
-    borderColor: '#00f',
-    borderWidth: 0,
-  },
-  contentContainer: {
-    borderColor: '#0f0',
-    borderWidth: 0,
-  },
-});
