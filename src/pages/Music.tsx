576cd822c6b6908fa492578f475651bff574e1c9
Daniel-Alencar <danielalencar746@gmail.com>
2021-01-25 13:40:08 -0300

    começando com styled -components

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
 import styled from 'styled-components';
+
+const Background = styled.View`
+  flex: 1;
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function Music() {
   return(
-    <View style={styles.container}>
+    <Background>
 
-    </View>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
-
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
