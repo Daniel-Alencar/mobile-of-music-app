9470629d46aa9e2f91d7662dc67362ff24a09057
Daniel-Alencar <danielalencar746@gmail.com>
2021-01-25 14:34:15 -0300

    TopBar concluída utilizando styled components

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import ChevronIcon from '../components/MusicScreen/ChevronIcon';
+import MoreVertIcon from '../components/MusicScreen/MoreVertIcon';
+
 const Background = styled.View`
   flex: 1;
   background-color: grey;
-
   padding-top: 50px;
 `;
 
 const TopBar = styled.View`
   border-color: #f00;
-  border-width: 3;
+  border-width: 0;
+
+  flex-direction: row;
 `;
+
+TopBar.Left = styled.View`
+  flex: 1;
+  justify-content: center;
+
+  padding-left: 16px;
+`;
+TopBar.Middle = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
+TopBar.Right = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: flex-end;
+
+  padding-right: 16px;
+`;
+
 TopBar.Title = styled.Text`
   color: white;
+  text-transform: uppercase;
+
 `;
 TopBar.SubTitle = styled.Text`
   color: white;
+  font-weight: bold;
 `;
 
 export default function Music() {
@@ -24,16 +51,22 @@ export default function Music() {
     <Background>
       <TopBar>
 
-        <ChevronIcon />
+        <TopBar.Left>
+          <ChevronIcon />
+        </TopBar.Left>
 
-        <TopBar.Title>
-          Tocando podcast
-        </TopBar.Title>
-        <TopBar.SubTitle>
-          Hipsters Ponto Tech
-        </TopBar.SubTitle>
+        <TopBar.Middle>
+          <TopBar.Title>
+            Tocando podcast
+          </TopBar.Title>
+          <TopBar.SubTitle>
+            Hipsters Ponto Tech
+          </TopBar.SubTitle>
+        </TopBar.Middle>
 
-        <MoreVertIcon />
+        <TopBar.Right>
+          <MoreVertIcon />
+        </TopBar.Right>
 
       </TopBar>
     </Background>
