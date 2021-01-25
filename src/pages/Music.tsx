f25ea8cb0b5e4d54dabcae95fd8c1c11560006d8
Daniel-Alencar <danielalencar746@gmail.com>
2021-01-25 14:07:50 -0300

    Colocando o react native para ler SVG files

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -3,15 +3,39 @@ import styled from 'styled-components';
 
 const Background = styled.View`
   flex: 1;
-  background-color: black;
-  align-items: center;
-  justify-content: center;
+  background-color: grey;
+
+  padding-top: 50px;
+`;
+
+const TopBar = styled.View`
+  border-color: #f00;
+  border-width: 3;
+`;
+TopBar.Title = styled.Text`
+  color: white;
+`;
+TopBar.SubTitle = styled.Text`
+  color: white;
 `;
 
 export default function Music() {
   return(
     <Background>
+      <TopBar>
 
+        <ChevronIcon />
+
+        <TopBar.Title>
+          Tocando podcast
+        </TopBar.Title>
+        <TopBar.SubTitle>
+          Hipsters Ponto Tech
+        </TopBar.SubTitle>
+
+        <MoreVertIcon />
+
+      </TopBar>
     </Background>
   );
 }
