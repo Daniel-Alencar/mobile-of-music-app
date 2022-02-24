a1c948fa0867bb3e68775d239dcf72f5d4124d86
Daniel-Alencar <danielalencar746@gmail.com>
2022-02-24 12:47:16 -0300

    Fazendo autenticação com o DropBox

--- src/pages/LoginScreen/index.tsx
+++ src/pages/LoginScreen/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Linking } from 'react-native';
 import { View, TouchableOpacity, Text } from 'react-native';
+
+import { Dropbox } from 'dropbox';
 
 import styles from './style';
 
 export default function LoginScreen() {
 
-  const link = "https://www.dropbox.com/oauth2/authorize?client_id=bupw6noqoenygsa&redirect_uri=https://translate.google.com.br/&response_type=code";
+  const _handleOpenURL = (ev: any) => {
+    console.log(ev);
+  }
 
-  function handleSignIn() {
-    Linking.openURL(link);
+  const openDropBox = () => {
+    var dbx = new Dropbox({ accessToken: 'YOUR_ACCESS_TOKEN_HERE' });
+    dbx.filesListFolder({path: ''})
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    Linking.getInitialURL().then((ev) => {
+      if (ev) {
+        _handleOpenURL(ev);
+      }
+    }).catch(err => {
+        console.warn('An error occurred', err);
+    });
+    Linking.addEventListener('exp://10.0.0.37:19000', _handleOpenURL);
+  }, []);
+
+  const link = "https://www.dropbox.com/oauth2/authorize?client_id=bupw6noqoenygsa&redirect_uri=exp://10.0.0.37:19000&response_type=token";
+
+  async function handleSignIn() {
+    await Linking.openURL(link);
   }
 
   return(
