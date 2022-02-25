800e011cadaf1bcb8b586353af8e33343b0bb90d
Daniel-Alencar <danielalencar746@gmail.com>
2022-02-25 10:51:18 -0300

    Fazendo login e recuperando dadso

--- src/pages/LoginScreen/index.tsx
+++ src/pages/LoginScreen/index.tsx
@@ -1,50 +1,84 @@
-import React, { useEffect } from 'react';
-import { Linking } from 'react-native';
+import React, { useState } from 'react';
+import * as Linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser';
 import { View, TouchableOpacity, Text } from 'react-native';
-
-import { Dropbox } from 'dropbox';
+import Constants from 'expo-constants';
 
 import styles from './style';
 
 export default function LoginScreen() {
 
-  const _handleOpenURL = (ev: any) => {
-    console.log(ev);
-  }
+  const [redirectedData, setRedirectedData] = useState<Linking.ParsedURL>();
 
-  const openDropBox = () => {
-    var dbx = new Dropbox({ accessToken: 'YOUR_ACCESS_TOKEN_HERE' });
-    dbx.filesListFolder({path: ''})
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+  const link = "https://www.dropbox.com/oauth2/authorize?client_id=bupw6noqoenygsa&response_type=token";
+  const redirect_uri = Linking.createURL("");
+
+  const full_link = link + '&redirect_uri=' + redirect_uri;
+
+
+
+
+
+
+
+
+
+  
+  const redirectData = () => {
+    if(redirectedData) {
+      return (
+        <Text style={{ marginTop: 30 }}>
+          {JSON.stringify(redirectedData)}
+        </Text>
+      );
+    }
+    return "NADA";
   };
 
-  useEffect(() => {
-    Linking.getInitialURL().then((ev) => {
-      if (ev) {
-        _handleOpenURL(ev);
-      }
-    }).catch(err => {
-        console.warn('An error occurred', err);
-    });
-    Linking.addEventListener('exp://10.0.0.37:19000', _handleOpenURL);
-  }, []);
 
-  const link = "https://www.dropbox.com/oauth2/authorize?client_id=bupw6noqoenygsa&redirect_uri=exp://10.0.0.37:19000&response_type=token";
 
-  async function handleSignIn() {
-    await Linking.openURL(link);
-  }
+
+
+
+  const _handleRedirect = (event: any) => {
+    if (Constants.platform?.ios) {
+      WebBrowser.dismissBrowser();
+    } else {
+      _removeLinkingListener();
+    }
+
+    console.log(event.url)
+    let data = Linking.parse(event.url);
+    setRedirectedData(data);
+  };
+
+  const _addLinkingListener = () => {
+    Linking.addEventListener("url", _handleRedirect);
+  };
+  
+  const _removeLinkingListener = () => {
+    Linking.removeEventListener("url", _handleRedirect);
+  };
+  
+  const _openBrowserAsync = async () => {
+    _addLinkingListener();
+    await WebBrowser.openBrowserAsync(full_link);
+
+    if (Constants.platform?.ios) {
+      _removeLinkingListener();
+    }
+  };
+
+
 
   return(
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+      <TouchableOpacity style={styles.button} onPress={_openBrowserAsync}>
         <Text>Login</Text>
       </TouchableOpacity>
+      <Text>
+        {redirectData()}
+      </Text>
     </View>
   );
 }
